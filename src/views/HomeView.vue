e1fc93af3f738619ad912088aa04f979e228fa5c
phumchaem <121370643+phumchaem@users.noreply.github.com>
2022-12-25 12:27:30 +0700

    add all

---
+++ src/views/HomeView.vue
@@ -0,0 +1,39 @@
+<script setup>
+import About from '../components/section/About.vue';
+import Facts from '../components/section/Facts.vue';
+import Skills from '../components/section/Skills.vue';
+import Resume from '../components/section/Resume.vue';
+import Portfolio from '../components/section/Portfolio.vue';
+import Services from '../components/section/Services.vue';
+import Testimonials from '../components/section/Testimonials.vue';
+import Contact from '../components/section/Contact.vue';
+
+</script>
+
+<template>
+      <main id="main">
+
+    <!-- ======= About Section ======= -->
+    <About></About>
+    <!-- End About Section -->
+
+    
+
+    <!-- ======= Skills Section ======= -->
+    <Skills></Skills>
+    <!-- End Skills Section -->
+
+    <!-- ======= Resume Section ======= -->
+    <Resume></Resume>
+    <!-- End Resume Section -->
+
+    <!-- ======= Portfolio Section ======= -->
+    <Portfolio></Portfolio>
+    <!-- End Portfolio Section -->
+
+    <!-- ======= Contact Section ======= -->
+    <Contact></Contact>
+    <!-- End Contact Section -->
+
+  </main><!-- End #main -->
+</template>
